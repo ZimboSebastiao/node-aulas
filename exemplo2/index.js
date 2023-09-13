cf7429d2cb45cac9b8ac30fc8c0a54b6b1c91ed3
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-13 09:06:06 -0300

    Update index.js

--- exemplo2/index.js
+++ exemplo2/index.js
@@ -1,3 +1,15 @@
 function soma(valor1, valor2) {
-    
-}
+    return valor1 + valor2;
+
+} 
+
+function multiplica(valor1, valor2) {
+    return valor1 * valor2;
+
+} 
+
+resultadoSoma = soma(10, 50);
+resultadoMultiplica = multiplica(2, 10);
+
+console.log(resultadoSoma);
+console.log(resultadoMultiplica);
