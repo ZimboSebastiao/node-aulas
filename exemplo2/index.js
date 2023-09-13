6b7d8e1fd563947cb8fbd33b7bc2898b36d412ca
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-13 09:08:47 -0300

    Update index.js

--- exemplo2/index.js
+++ exemplo2/index.js
@@ -11,5 +11,5 @@ function multiplica(valor1, valor2) {
 let resultadoSoma = soma(10, 50);
 let resultadoMultiplica = multiplica(2, 10);
 
-console.log(resultadoSoma);
-console.log(resultadoMultiplica);
+console.log(`Soma: ${resultadoSoma}`);
+console.log(`Multiplicação ${resultadoMultiplica}`);
