d560d9bd7f9c92dd3ba16cdb177e97c46c55efbb
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-13 09:46:39 -0300

    criação do arquivo package.json

--- exemplo2/modulos/funcoes.mjs
+++ exemplo2/modulos/funcoes.mjs
@@ -13,3 +13,7 @@ function divide(valor1, valor2) {
 function subtrai(valor1, valor2) {
     return valor1 - valor2;
 } 
+
+// export default soma;  // Para exportar um único recurso
+
+export {soma, multiplica, divide, subtrai}; // Para exportar diversos recursos
