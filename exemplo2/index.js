67497bad6cffc111dfcda161ce76e5eafb9b76d5
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-13 09:49:42 -0300

    Update index.js

--- exemplo2/index.js
+++ exemplo2/index.js
@@ -1,12 +1,13 @@
-import soma from './modulos/funcoes.mjs';
+// import soma from './modulos/funcoes.mjs'; // Importando um único recurso
+import {soma, multiplica, divide, subtrai} from './modulos/funcoes.mjs';
 
 
 let resultadoSoma = soma(10, 50);
-// let resultadoMultiplica = multiplica(2, 10);
-// let resultadoDivide = divide(50, 5);
-// let resultadoSubtrai = subtrai(10, 4);
+let resultadoMultiplica = multiplica(2, 10);
+let resultadoDivide = divide(50, 5);
+let resultadoSubtrai = subtrai(10, 4);
 
 console.log(`Soma: ${resultadoSoma}`);
-// console.log(`Multiplicação: ${resultadoMultiplica}`);
-// console.log(`Divisão: ${resultadoDivide}`);
-// console.log(`Subtração: ${resultadoSubtrai}`);
+console.log(`Multiplicação: ${resultadoMultiplica}`);
+console.log(`Divisão: ${resultadoDivide}`);
+console.log(`Subtração: ${resultadoSubtrai}`);
