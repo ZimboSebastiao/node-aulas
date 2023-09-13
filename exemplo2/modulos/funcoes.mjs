21b69728efb4193794a35386d91c9c0598b011ba
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-13 09:21:54 -0300

    criação do modulo de funcoes

---
+++ exemplo2/modulos/funcoes.mjs
@@ -0,0 +1,15 @@
+function soma(valor1, valor2) {
+    return valor1 + valor2;
+} 
+
+function multiplica(valor1, valor2) {
+    return valor1 * valor2;
+} 
+
+function divide(valor1, valor2) {
+    return valor1 / valor2;
+} 
+
+function subtrai(valor1, valor2) {
+    return valor1 - valor2;
+} 
