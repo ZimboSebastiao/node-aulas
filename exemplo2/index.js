a0308ff55b6611a2a7b73d5c41960c5255d94a74
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-13 09:15:32 -0300

    Função de divisão e subtração

--- exemplo2/index.js
+++ exemplo2/index.js
@@ -1,15 +1,25 @@
 function soma(valor1, valor2) {
     return valor1 + valor2;
-
 } 
 
 function multiplica(valor1, valor2) {
     return valor1 * valor2;
+} 
 
+function divide(valor1, valor2) {
+    return valor1 / valor2;
+} 
+
+function subtrai(valor1, valor2) {
+    return valor1 - valor2;
 } 
 
 let resultadoSoma = soma(10, 50);
 let resultadoMultiplica = multiplica(2, 10);
+let resultadoDivide = divide(50, 5);
+let resultadoSubtrai = subtrai(10, 4);
 
 console.log(`Soma: ${resultadoSoma}`);
-console.log(`Multiplicação ${resultadoMultiplica}`);
+console.log(`Multiplicação: ${resultadoMultiplica}`);
+console.log(`Divisão: ${resultadoDivide}`);
+console.log(`Subtração: ${resultadoSubtrai}`);
