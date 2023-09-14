0883f4296947108588a9319d4e403babc497f93f
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-14 10:52:02 -0300

    update

--- exemplo4/servidor.js
+++ exemplo4/servidor.js
@@ -1,27 +1,45 @@
-import http from 'http'; // importabdo o módulo nativo http, mesmo sendo um módulo nativo, ainda assim precisamod do package.json
+import http from 'http'; // Importando módulo nativo http
+import fs from 'fs/promises'; // permite ao servidor acesso ao sistema de arquivos (módulo nativo)
 
-// Craiando uma função que vai monitorar as requisições e respostas do servidor. Recebe dois parâmetros, Requisição e Resposta
-const monitorRequisicao = (req, res) => {
+// constante que monitora requisiçoes e recebe dois parâmetros req (requisição) e res (resposta)
 
-    // Configurando o cabeçalho da resposta para trabalhar com arquivos HTML e charset-utf-8
-    res.writeHead(200, {"Contet-Type" : "text/html;charset=utf8"});
+const monitoraRequisicao = (req, res) => { // arrow function
+    // configurando o cabeçalho da resposta para trabalhar com arquivos html e charcet-utf8
+    res.writeHead(200, {"Content-Type" : "text/html;charset=utf8"});
 
-
-
-    // Avaliando cada requisição (a partir do link)
     switch (req.url) {
         case '/':
-            // o carregamento e leitura do index.html
-        break;
+            // exibir index.html
+            // res.end('Servidor Node rodando no navegador.');
+
+            // fs faz o carregamento e leitura do arquivo
+            fs.readFile('paginas/index.html')
+
+            // depois de pronto
+            .then(conteudo => res.end(conteudo));
+            break;
 
         case '/sobre':
-            // o carregamento e leitura do sobre.html
-        break;
+            // exibir sobre.html
 
+            fs.readFile('paginas/sobre.html').then(conteudo => res.end(conteudo));
+            break;
+    
         default:
-            // o carregamento e leitura do 404.html
-        break;
+            // exibir 404.html
+            res.writeHead(404);
+            fs.readFile('paginas/404.html').then(conteudo => res.end(conteudo));
+            break;
     }
+};
+
+// Criando o servidor http e indicando a função de requisições
+const servidor = http.createServer(monitoraRequisicao);
+
+// iniciando a escuta do servidor local na porta 8080
+// servidor.listen(8080); // só esse trecho seria suficiente, mas é importante rodar a mensagem para ter certeza que tudo deu certo
 
 
-}
+servidor.listen(8080, () => {
+    console.log("Servidor node rodando normalmente");
+});
