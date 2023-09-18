385d1927b6ad0d04a9beece9091135f1660c9bfa
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-18 10:12:49 -0300

    update

--- exercicio2/app.js
+++ exercicio2/app.js
@@ -18,6 +18,15 @@ app.get('/404', (req, res) => {
     res.render('paginas/404');
 });
 
+// Rota genérica para capturar todas as outras solicitações GET não tratadas
+// app.get('*', (req, res) => {
+//     res.redirect('/404');
+// });
+
+app.use((req, res) => {
+    res.render('paginas/404');
+});
+
 // Inicializar o servidor
 app.listen(3000, () => {
     console.log('Servidor rodando normalmente');
